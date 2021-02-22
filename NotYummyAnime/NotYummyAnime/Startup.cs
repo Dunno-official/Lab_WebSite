696a861ed8421dd65a647a10644411a4c62ef4e5
Denis Bondar <60221683+Dunno-official@users.noreply.github.com>
2021-02-22 20:17:48 +0200

    .

--- NotYummyAnime/NotYummyAnime/Startup.cs
+++ NotYummyAnime/NotYummyAnime/Startup.cs
@@ -54,7 +54,7 @@ namespace NotYummyAnime
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Genres}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
